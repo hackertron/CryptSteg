5b8331ad1d3bc07c95f1a5ae29190a2e50f7dc68
jayssj11 <cooljay.gupta@gmail.com>
2016-02-10 09:55:35 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,57 @@
+<?php 
+  $dbhost  = 'localhost';    // Unlikely to require changing
+  $dbname  = 'crypt';   // Modify these...
+  $dbuser  = 'root';   // ...variables according
+  $dbpass  = 'blah';   // ...to your installation
+  $appname = "CryptSteg"; // ...and preference
+
+  $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  if ($connection->connect_error) die($connection->connect_error);
+
+  function createTable($name, $query)
+  {
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' created or already exists.<br>";
+  }
+
+  function queryMysql($query)
+  {
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+  }
+
+  function destroySession()
+  {
+    $_SESSION=array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()]))
+      setcookie(session_name(), '', time()-2592000, '/');
+
+    session_destroy();
+  }
+
+  function sanitizeString($var)
+  {
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $connection->real_escape_string($var);
+  }
+
+  function showProfile($user)
+  {
+    if (file_exists("$user.jpg"))
+      echo "<img src='$user.jpg' style='float:left;'>";
+
+    $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows)
+    {
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    }
+  }
+?>
